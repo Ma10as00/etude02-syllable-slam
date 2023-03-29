2a4dc325bf42dc86bb742d767c75e862740e7630
Kurt <wedku875@student.otago.ac.nz>
2023-03-30 10:29:24 +1300

    Combined the syllable counter and the syllable finder into one file

--- Syllable2.java
+++ Syllable2.java
@@ -14,6 +14,7 @@ public class Syllable2 {
                 String[] words = sc.nextLine().split("=");
                 hm.put(words[0], words[1].split("-").length);
             }
+            sc.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
@@ -24,8 +25,54 @@ public class Syllable2 {
             if (hm.containsKey(word)) {
                 System.out.println(hm.get(word));
             } else {
-                System.err.println("Word not found");
+                System.out.println(countSyllables(word));
             }
         }
+        scan.close();
+    }
+
+    /**
+     * Counts the number of syllables in a word.
+     * Iterates through each letter to see if the prior letter was a vowel, if there were 2 in a row the second is not counted. The third is.
+     * @param word the word to be counted
+     * @return the number of syllables in the word
+     */
+    public static int countSyllables(String word){
+        int syllables = 0;
+        for(int i = 0; i < word.length(); i++){
+            // Counting a vowel for syllables if there are three in a row (this ends up counting the three vowels as two syllables)
+            try{
+                if (isVowel(word.charAt(i)) && isVowel(word.charAt(i-1)) && isVowel(word.charAt(i-2))){
+                    countSyl(i);
+                    syllables++;
+                    continue;
+                }
+            } catch(StringIndexOutOfBoundsException e){
+                System.err.println("String Index out of bounds error on the letter at position: " + i + ", " + word.charAt(i));
+            }
+
+
+            // Not counting a letter if it is preceded by only 1  vowel
+            try{
+                if (isVowel(word.charAt(i)) && isVowel(word.charAt(i-1))) continue;
+            } catch(StringIndexOutOfBoundsException e){
+                System.err.println("String Index out of bounds error on the letter at position: " + i + ", " + word.charAt(i));
+                countSyl(i);
+                syllables++;
+            }
+            if (isVowel(word.charAt(i)) && i > 0) {
+                countSyl(i);
+                syllables++;
+            }
+        }
+        return syllables;
+    }
+
+    public static void countSyl(int index){
+        //System.out.println("Counted a syllable at character: "+ index);
+    }
+
+    public static boolean isVowel(char c){
+        return "aeiouy".indexOf(c) != -1;
     }
 }
